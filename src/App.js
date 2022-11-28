1652197951676ad0a73e5a79d7de322e4019d384
uriiisegura <okiko22@gmail.com>
2022-11-28 17:59:47 +0100

    React import errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import DataProcessor from "./Components/DataProcessor";
 import DataFormatter from "./Components/DataFormatter";
 
--- src/Components/DataFormatter.js
+++ src/Components/DataFormatter.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./DataFormatter.css";
 
 function DataFormatter(props) {
