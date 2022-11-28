9d839961d5f0c0ddab5b1bf89e20d7f030f3a266
Andreu Huguet <andreuhuguet@gmail.com>
2022-11-28 15:48:40 +0100

    Prova d'agafar dades per consola

---
+++ get_data.js
@@ -0,0 +1,14 @@
+const link_dades = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQeAif6pgFuLUAXHif4IsrSXzG8itYhirTHGdmNzA5RmrEPcJe7lcfwfNVLBEcgnn3mZbThqaZdouiP/pub?gid=0&single=true&output=csv";
+
+function init() {
+  Papa.parse(link_dades, {
+    download: true,
+    header: true,
+    complete: function(results) {
+      var data = results.data
+      console.log(data)
+    }
+  });
+}
+
+window.addEventListener('DOMContentLoaded', init)
