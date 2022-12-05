a8addbffdf65245296d0532e13b0562d98ec8f06
Andreu Huguet <andreuhuguet@gmail.com>
2022-12-05 20:27:48 +0100

    Revert "Center Stats in mobile"
    This reverts commit 06805ed16318bc375a0554f4c4dd24809ff8c0e4.

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -45,9 +45,9 @@ function Stats(props) {
 	let castells_rows, castells_rows_names, desCar_rows, colles_rows, colles_rows_names, colles_rows_names_two, table_length;
 	let reset_rows = true;
 	return (
-		<div id="stats-container">
-            <h1>Estadístiques</h1>
+		<>
 			<div id="stats">
+            <h1>Estadístiques</h1>
 			{
 				castells_arrays.map((castell, i) => {
 					const name = castell[0];
@@ -124,7 +124,7 @@ function Stats(props) {
 				})
 			}
 			</div>
-		</div>
+		</>
 	);
 }
 
