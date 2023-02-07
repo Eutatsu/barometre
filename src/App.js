cb14ebd7a2b6a00c3b73b92ffcf75d52749255bc
uriiisegura <okiko22@gmail.com>
2023-02-07 16:57:34 +0100

    Change router type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import DataProcessor from "./Components/DataProcessor";
 import Barometre from './Components/Barometre'
