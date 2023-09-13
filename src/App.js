c910a8951466332484a57a35ce8d195bf3443b65
Oriol Segura Niño <okiko22@gmail.com>
2023-09-13 23:33:39 +0200

    HashRouter --> BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
 import DataProcessor from "./components/DataProcessor";
